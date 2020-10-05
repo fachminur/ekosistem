f41e614931ac99080cd703de6fc8baaccfa8fc73
Fahmi Nur Rahman <fachminur@gmail.com>
2020-10-05 11:34:28 +0700

    commit

--- latihan_api_php/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    $response['value'] = 1;
-    $response['message'] = 'Login Berhasil';
-    echo json_encode($response);
--- lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kBackgroundColor = Color(0xff263c92);
-const kDefaultPadding = 20.0;
